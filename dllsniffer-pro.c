304dbdc1cbdefa4a1fca174d788821b011784c1a
faezehghiasi <f.ghiasi@ec.iut.ir>
2024-12-12 23:55:49 +0330

    some changes

--- dllsniffer-pro.c
+++ dllsniffer-pro.c
@@ -11,18 +11,24 @@
 #include <errno.h>
 #include <netinet/ip.h> 
 
-int main() {
+void print_mac_address(unsigned char *mac) {
+    printf("%02X:%02X:%02X:%02X:%02X:%02X", 
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+}
 
-     int sockfd;
+int main() {
+    int sockfd;
     sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sockfd < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
-    printf("Socket created successfully.\n");
+    printf("[+] Capturing packets...\n\n");
+
     char buffer[2048];
     struct sockaddr_ll phyaddr;
     socklen_t addr_len = sizeof(struct sockaddr_ll);
+
     while (1) {
         int numbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&phyaddr, &addr_len);
         if (numbytes < 0) {
@@ -30,48 +36,37 @@ int main() {
             close(sockfd);
             exit(EXIT_FAILURE);
         }
+
         struct ethhdr *eth = (struct ethhdr *)buffer;
         unsigned short protocol = ntohs(eth->h_proto);
 
-        if (protocol == ETH_P_IP) {
-            printf("Upper Protocol :IP ");
-            int eth_header_len = 14;
-            struct iphdr *ip = (struct iphdr *)(buffer + eth_header_len);
-            unsigned int ip_header_len = ip->ihl * 4;
-            unsigned short total_length = ntohs(ip->tot_len);
-            unsigned char upper_protocol = ip->protocol;
-            printf("(header len: %u , ", ip_header_len);
-            printf("total len: %u , ", total_length);
-            printf("proto : %u ), ", upper_protocol);
-            
-        } else if (protocol == ETH_P_ARP) {
-            printf("Upper Protocol :ARP , ");
-        } else {
-            printf("Incoming packet with unknown protocol: 0x%04x , ", protocol);
-        }
+        printf("Packet Type: ");
         switch (phyaddr.sll_pkttype) {
-            case PACKET_OUTGOING:
-                printf("Outgoing:\n");
-                break;
-            case PACKET_BROADCAST:
-                printf("Broadcast:\n");
-                break;
-            case PACKET_MULTICAST:
-                printf("Multicast:\n");
-                break;
-            case PACKET_HOST:
-                printf("Incoming:\n");
-                break;
-            default:
-                printf("Other:\n");
-                break;
+            case PACKET_OUTGOING: printf("Outgoing\n"); break;
+            case PACKET_BROADCAST: printf("Broadcast\n"); break;
+            case PACKET_MULTICAST: printf("Multicast\n"); break;
+            case PACKET_HOST: printf("Incoming\n"); break;
+            default: printf("Other\n"); break;
         }
+
+        printf("Source MAC: ");
+        print_mac_address(eth->h_source);
+        printf("\nDestination MAC: ");
+        print_mac_address(eth->h_dest);
+
+        if (protocol == ETH_P_IP) {
+            printf("\nProtocol: IP\n");
+        } else if (protocol == ETH_P_ARP) {
+            printf("\nProtocol: ARP\n");
+        } else {
+            printf("\nProtocol: Unknown (0x%04X)\n", protocol);
+        }
+
+        printf("Payload (Hex): ");
         for (int i = 0; i < numbytes; i++) {
-            printf("%02x ", (unsigned char)buffer[i]);
+            printf("%02X ", (unsigned char)buffer[i]);
         }
-
         printf("\n\n");
-
     }
     close(sockfd);
     return 0;
